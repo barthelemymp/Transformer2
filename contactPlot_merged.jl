8efb985cb075f0daf1707f8eef9c248242a888cb
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-02-22 00:09:46 +0100

    test inter sampling contacts

---
+++ contactPlot_merged.jl
@@ -0,0 +1,455 @@
+using Base.Threads: @threads, nthreads
+using ArDCA
+using DCAUtils
+using ArgParse
+using ExtractMacro: @extract
+using StatsBase
+using NPZ
+using PdbTool
+using PlmDCA
+
+
+function get_HMMlength(HmmPath)
+	a = String(read(`grep LENG $HmmPath`))
+	b = split(a, "  ")[2]
+	c = split(b, "\n")[1]
+	return parse(Int64,c)
+end
+
+function makeInterRoc(score::Array{Tuple{Int64,Int64,Float64},1},chain1::PdbTool.Chain,chain2::PdbTool.Chain;sz=200,cutoff::Float64=8.0,out::AbstractString="return",pymolMode::Bool=false,naccessRatio::Float64=1.0)
+
+		# Check if mapping is existent
+		if chain1.mappedTo == ""
+				error("chain 1 has no mapping")
+		elseif chain2.mappedTo == ""
+				error("chain 2 has no mapping")
+		end
+		LENG1=PdbTool.getHmmLength(chain1.mappedTo)
+		LENG2=PdbTool.getHmmLength(chain2.mappedTo)
+
+		# Get naccess cutoff if necessary
+		if naccessRatio<1.0
+			naList1=zeros(LENG1);
+			naList2=zeros(LENG2);
+			i=1;
+			for r1 in values(chain1.align)
+				naList1[i]=r1.naccess;
+				i+=1;
+			end
+			i=1;
+			for r2 in values(chain2.align)
+				naList2[i]=r2.naccess;
+				i+=1;
+			end
+			naList1=sort(naList1,rev=true);
+			na1Cutoff=naList1[parse(Int64,round(LENG1*naccessRatio))];
+			println(na1Cutoff)
+			naList2=sort(naList2,rev=true);
+			na2Cutoff=naList2[parse(Int64,round(LENG2*naccessRatio))];
+			println(na2Cutoff)
+		end
+
+		if out=="return"
+			roc=Array{Tuple{AbstractString,AbstractString,Float64,Float64}}(undef,0)
+			s::Int64=0
+			i::Int64=0
+			hits::Int64=0
+			positives::Int64=0
+			while s<sz && i<size(score,1)
+				i+=1
+				if score[i][1] <= LENG1 && score[i][2] > LENG1
+					ind1=score[i][1]
+					ind2=score[i][2]-LENG1
+				elseif score[i][2] <= LENG1 && score[i][1] > LENG1
+					ind1=score[i][2]
+					ind2=score[i][1]-LENG1
+				else
+					continue
+				end
+				if haskey(chain1.align,ind1) && haskey(chain2.align,ind2)
+					if naccessRatio<1.0
+						if chain1.align[ind1].naccess<na1Cutoff || chain2.align[ind2].naccess<na2Cutoff
+							continue;
+						end
+					end
+					s+=1
+					id1=chain1.align[ind1].identifier
+					id2=chain2.align[ind2].identifier
+					if PdbTool.residueDist(chain1.align[ind1],chain2.align[ind2])<cutoff
+						hits+=1
+						if pymolMode
+							hits=s
+						end
+						push!(roc,(id1,id2,hits/s,score[i][3]))
+					else
+						if pymolMode
+							hits=0
+						end
+						push!(roc,(id1,id2,hits/s,score[i][3]))
+					end
+				end
+			end
+			return roc
+		end
+	end
+
+
+function mergedPDBInterRoc(score::Array{Tuple{Int64,Int64,Float64},1},chain1_list,chain2_list;sz=200,cutoff::Float64=8.0,out::AbstractString="return",pymolMode::Bool=false,naccessRatio::Float64=1.0)
+	# Check if mapping is existent
+	for p in 1:length(chain1_list)
+		chain1 = chain1_list[p]
+		chain2 = chain2_list[p]
+		if chain1.mappedTo == ""
+				error("chain 1 has no mapping")
+		elseif chain2.mappedTo == ""
+				error("chain 2 has no mapping")
+		end
+	end
+	LENG1=PdbTool.getHmmLength(chain1_list[1].mappedTo)
+	LENG2=PdbTool.getHmmLength(chain2_list[1].mappedTo)
+	roc=Array{Tuple{Float64,Float64}}(undef,0)
+	s::Int64=0
+	i::Int64=0
+	hits::Int64=0
+	positives::Int64=0
+	while s<sz && i<size(score,1)
+		i+=1
+		tempContact::Int64 = 0
+		tempHaskey::Int64 = 0
+		if score[i][1] <= LENG1 && score[i][2] > LENG1
+			ind1=score[i][1]
+			ind2=score[i][2]-LENG1
+		elseif score[i][2] <= LENG1 && score[i][1] > LENG1
+			ind1=score[i][2]
+			ind2=score[i][1]-LENG1
+		else
+			continue
+		end
+		for p in 1:length(chain1_list)
+			chain1 = chain1_list[p]
+			chain2 = chain2_list[p]
+			if haskey(chain1.align,ind1) && haskey(chain2.align,ind2)
+				tempHaskey +=1
+				# s+=1
+				id1=chain1.align[ind1].identifier
+				id2=chain2.align[ind2].identifier
+				if PdbTool.residueDist(chain1.align[ind1],chain2.align[ind2])<cutoff
+					tempContact+=1
+				end
+			end
+		end
+		if tempHaskey>0
+			s+=1
+			if tempContact>=1
+				hits+=1
+				push!(roc,(hits/s,score[i][3]))
+			else
+				push!(roc,(hits/s,score[i][3]))
+			end
+		end
+	end
+	return roc
+end
+
+
+function ppv_mergedcontact(file::String, score, sequence_length1::Int, sequence_length2::Int;contact_dist::Float64=10.0, sizeppv::Int=200)
+    A = DelimitedFiles.readdlm(file, ',')
+    l, _ =size(A)
+    contacts = []#Array{Int, 2}
+    distmat = Inf.*ones(sequence_length1,sequence_length2)
+    for i=2:l
+		@show i, A[i,:]
+        indi = A[i,1]+1
+        indj =A[i,2]+1
+		if A[i,4] != ""
+			@show A[i,4]
+			@show indi, indj
+        	distmat[indi,indj] = min(distmat[indi,indj], A[i,4])
+		end
+    end
+	y = zeros(sizeppv)
+	goodpred = 0
+    for l=1:sizeppv
+		i = score[l][1]
+		j = score[l][2]
+        if distmat[i,j]<contact_dist
+			goodpred +=1
+        end
+		push!(y,  goodpred/l)
+    end
+    return y
+end
+
+
+function ppv(results; size::Int=200)
+	y = zeros(size)
+	goodPred=0
+	for l in 1:size
+		goodPred+=results[l][3]
+		y[l] = goodPred/l
+	end
+	return y
+end
+
+
+
+
+
+
+
+
+
+
+"/home/feinauer/Datasets/DomainsInter/filters/new_training_set_ddi_num_17_PF03171_PF14226.dat"
+
+
+
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+	"pathfastatrain"
+        help = "MSA path train"
+		arg_type = String
+        required = true
+
+	"pathPDB"
+        help = "PDB_list"
+		arg_type = String
+
+	"chainIN"
+        help = "chain for imput list"
+		arg_type = String
+
+	"chainOUT"
+	    help = "chain output list"
+		arg_type = String
+	    required = true
+
+	"hmmRadical"
+        help = "PDB path"
+		arg_type = String
+        required = true
+
+	"outputpath"
+        help = "path for plot output"
+		arg_type = String
+        required = true
+
+	"mode"
+        help = "mode of dca: inter or intra"
+		arg_type = String
+        required = true
+
+    end
+    return parse_args(s)
+end
+
+parsed_args = parse_commandline()
+println("Parsed args:")
+for (arg,val) in parsed_args
+    println("  $arg  =>  $val")
+end
+
+pathfastatrain = parsed_args["pathfastatrain"]
+mode = parsed_args["mode"]
+
+using PyCall
+np = pyimport("numpy")
+
+PDBs = np.load("pdblisttemp.npy", allow_pickle=true)
+chainIN = np.load("chain1listtemp.npy", allow_pickle=true)
+chainOUT = np.load("chain2listtemp.npy", allow_pickle=true)
+
+hmmRadical = parsed_args["hmmRadical"]
+hmm1 = hmmRadical * "1.hmm"
+hmm2 = hmmRadical * "2.hmm"
+hmmjoined = hmmRadical * "joined.hmm"
+outputpath = parsed_args["outputpath"]
+
+
+
+
+
+
+
+if mode == "inter"
+	write("temp2.fasta",read(`./Unalign  $pathfastatrain`))
+	write("temp1.fasta",read(`hmmalign --outformat a2m $hmmjoined temp2.fasta`))
+	write("temp2.fasta",read(`./removeInserts temp1.fasta`))
+
+	plmo = plmdca_asym(joinpath(pwd(), "temp2.fasta"), theta = :auto)
+	chain1_list = []
+	chain2_list = []
+	for i in 1:length(PDBs)
+		comm = "https://files.rcsb.org/download/" * PDBs[i] * ".pdb"
+		if  !isfile(PDBs[i]*".pdb")
+			run(`wget $comm`)
+		pdb=PdbTool.parsePdb(PDBs[i]*".pdb")
+		pdb2=PdbTool.parsePdb(PDBs[i]*".pdb")
+		PdbTool.mapChainToHmm(pdb.chain[chainIN[i]], hmm1)
+		PdbTool.mapChainToHmm(pdb2.chain[chainOUT[i]], hmm2)
+		push!(chain1_list, pdb.chain[chainIN[i]])
+		push!(chain2_list, pdb2.chain[chainOUT[i]])
+	end
+	result = mergedPDBInterRoc( plmo.score,chain1_list, chain2_list;cutoff=10.0, sz=200)
+	ppv = map(x->x[1],result)
+	NPZ.write(outputpath, ppv)
+	# else
+	# 	sequence_length1 =  get_HMMlength(hmm1)
+	# 	sequence_length2 =  get_HMMlength(hmm2)
+	# 	y = ppv_mergedcontact(pathPDB, plmo.score, sequence_length1, sequence_length2;contact_dist=8.0, sizeppv=200)
+
+elseif mode == "intra"
+	write("temp2.fasta",read(`./Unalign  $pathfastatrain`))
+	write("temp1.fasta",read(`hmmalign --outformat a2m $hmm2 temp2.fasta`))
+	write("temp2.fasta",read(`./removeInserts temp1.fasta`))
+	pdb=PdbTool.parsePdb(pathPDB)
+	PdbTool.mapChainToHmm(pdb.chain[chainOUT], hmm2)
+	plmo = plmdca_asym(joinpath(pwd(), "temp2.fasta"), theta = :auto)
+	result = makeIntraRoc(plmo.score,pdb.chain[chainOUT])
+end
+
+#
+#
+#
+# pathfastatrainjoined = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_train_joined.faa"
+# pathfastatrain1 = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_train_1.faa"
+# pathfastatrain2 = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_train_2.faa"
+#
+# pathfastatrainjoined = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_trainjoined.faa"
+# pathfastatrain1 = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_train1.faa"
+# pathfastatrain2 = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_train2.faa"
+#
+# run(`hmmbuild --symfrac 0.0 hmm_972_1.hmm $pathfastatrain1`)
+# run(`hmmbuild --symfrac 0.0 hmm_972_2.hmm $pathfastatrain2`)
+# run(`hmmbuild --symfrac 0.0 hmm_972_joined.hmm $pathfastatrainjoined`)
+#
+# run(`wget https://files.rcsb.org/download/2VBP.pdb`)
+# pathPDB = "1BNC.pdb"
+# chainIN = "A"
+# chainOUT = "A"
+# pdb=PdbTool.parsePdb(pathPDB)
+# PdbTool.mapChainToHmm(pdb.chain[chainOUT], "hmm_972_2.hmm")
+# pdb.chain[chainOUT]
+# plmo = plmdca_asym(joinpath(pwd(), pathfastatrain2), theta = :auto)
+#
+#
+# sequence_length1 =  get_HMMlength("hmm_972_1.hmm")
+# sequence_length2 =  get_HMMlength("hmm_972_2.hmm")
+# ppv_mergedcontact(pathcontacts,plmo. score, sequence_length1, sequence_length2;contact_dist=0.0, sizeppv=200)
+# result = PdbTool.makeIntraRoc(plmo.score,pdb.chain[chainOUT])
+#
+#
+# plmo = plmdca_asym(joinpath(pwd(), "temp2.fasta"), theta = :auto)
+# result = PdbTool.makeIntraRoc(plmo.score,pdb.chain[chainOUT])
+#
+# pathsample = "sample_972_2.faa"
+# write("temp2.fasta",read(`./Unalign  $pathsample`))
+# write("temp1.fasta",read(`hmmalign --outformat a2m hmm_972_2.hmm temp2.fasta`))
+# write("temp2.fasta",read(`./removeInserts temp1.fasta`))
+# plmo = plmdca_asym(joinpath(pwd(), "temp2.fasta"), theta = :auto)
+# result = PdbTool.makeIntraRoc(plmo.score,pdb.chain[chainOUT])
+#
+#
+#
+#
+#
+#
+#
+#
+# pathfastatrainjoined
+# pathPDB = "2VBP.pdb"
+# chainIN = "A"
+# chainOUT = "A"
+# pdb=PdbTool.parsePdb(pathPDB)
+# pdb2=PdbTool.parsePdb(pathPDB)
+# PdbTool.mapChainToHmm(pdb.chain[chainIN], "hmm_972_1.hmm")
+# PdbTool.mapChainToHmm(pdb2.chain[chainOUT], "hmm_972_2.hmm")
+# plmo = plmdca_asym(joinpath(pwd(), pathfastatrainjoined), theta = :auto)
+# result = makeInterRoc(plmo.score,pdb.chain[chainIN],pdb2.chain[chainOUT])
+#
+#
+#
+# pathsample = "sample_972_joined.faa"
+#
+# plmo = plmdca_asym(joinpath(pwd(),pathsample), theta = :auto)
+# result = makeInterRoc(plmo.score,pdb.chain[chainIN],pdb2.chain[chainOUT])
+#
+#
+# pathsample = "sample_972_joined.faa"
+# write("temp2.fasta",read(`./Unalign  $pathsample`))
+# write("temp1.fasta",read(`hmmalign --outformat a2m hmm_972_joined.hmm temp2.fasta`))
+# write("temp2.fasta",read(`./removeInserts temp1.fasta`))
+# plmo = plmdca_asym(joinpath(pwd(), "temp2.fasta"), theta = :auto)
+# pdb2=PdbTool.parsePdb(pathPDB)
+# result = mergedPDBInterRoc( plmo.score,chain1_list, chain2_list;cutoff=12.0, sz=200)
+#
+#
+# write("temp2.fasta",read(`./Unalign  $pathfastatrainjoined`))
+# write("temp1.fasta",read(`hmmalign --outformat a2m hmm_972_joined.hmm temp2.fasta`))
+# write("temp2.fasta",read(`./removeInserts temp1.fasta`))
+# plmo = plmdca_asym(joinpath(pwd(), "temp2.fasta"), theta = :auto)
+#
+#
+# pathcontacts = "/home/feinauer/Datasets/DomainsInter/new_training_set_ddi_num_972_PF02785_PF00289.dat"
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# PDBs = NPZ.load("pdblisttemp.npy")
+#
+# chainIN = NPZ.load("chain1listtemp.npy")
+# chainOUT = NPZ.load("chain1listtemp.npy")
+#
+#
+#
+#
+# using PyCall
+#
+# np = pyimport("numpy")
+#
+# PDBs = np.load("pdblisttemp.npy", allow_pickle=true)
+#
+# chainIN = np.load("chain1listtemp.npy", allow_pickle=true)
+#
+# chainOUT = np.load("chain2listtemp.npy", allow_pickle=true)
+#
+#
+# pathfastatrainjoined = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_trainjoined.faa"
+# pathfastatrain1 = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_train1.faa"
+# pathfastatrain2 = "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_972_joined_train2.faa"
+# pathsample = "sample_972_joined.faa"
+#
+# plmo = plmdca_asym(joinpath(pwd(), "temp2.fasta"), theta = :auto)
+# chain1_list = []
+# chain2_list = []
+# hmm1 = "hmm_972_1.hmm"
+# hmm2 = "hmm_972_2.hmm"
+# for i in 1:length(PDBs)
+# 	comm = "https://files.rcsb.org/download/" * PDBs[i] * ".pdb"
+# 	run(`wget $comm`)
+# 	pdb=PdbTool.parsePdb(PDBs[i]*".pdb")
+# 	pdb2=PdbTool.parsePdb(PDBs[i]*".pdb")
+# 	PdbTool.mapChainToHmm(pdb.chain[chainIN[i]], hmm1)
+# 	PdbTool.mapChainToHmm(pdb2.chain[chainOUT[i]], hmm2)
+# 	push!(chain1_list, pdb.chain[chainIN[i]])
+# 	push!(chain2_list, pdb2.chain[chainOUT[i]])
+# end
---
+++ plotsamplongcontacts.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 18:24:01 2022
+
+@author: bartm
+"""
+import subprocess
+
+
+run(`hmmbuild --symfrac 0.0 hmm_972_1.hmm $pathfastatrain1`)
+
+
+def buildhmm(hmmout, ali):
+    subprocess.run(["hmmbuild", "--symfrac","0.0", hmmout, ali])
+
+
+
+
+def getlists(df, fam):
+    pdblist = list(df[df["id"]==fam]["pdb"])
+    chain1list = list(df[df["id"]==fam]["chain1"])
+    chain2list = list(df[df["id"]==fam]["chain2"])
+    return pdblist, chain1list, chain2list
+
+
+
+family_list = [1214,1213,1208,980,975,972,815,634,303,504,197,192,358,308,304,303,181,132,103,97,17]
+pdbtracker = pd.read_csv("pdbtracker.csv")
+
+
+import scipy.optimize
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+# from torch.utils.tensorboard import SummaryWriter
+from torchtext.legacy.data import Field, BucketIterator, TabularDataset
+from torch.utils.data import Dataset, DataLoader
+import sys
+import os
+import math
+import wandb
+sys.path.append("")
+from ProteinTransformer import *
+from ProteinsDataset import *
+from MatchingLoss import *
+from utils import *
+from ardca import *
+print("import done")
+#torch.functional.one_hot
+pathtoFolder = "/home/feinauer/Datasets/DomainsInter/processed/"
+torch.set_num_threads(4)
+#pathtoFolder = "/home/Datasets/DomainsInter/processed/"
+# Model hyperparameters--> CAN BE CHANGED
+batch_size = 32
+num_heads = 1
+num_encoder_layers = 2
+num_decoder_layers = 2
+dropout = 0.10
+forward_expansion = 2048
+src_vocab_size = 25#len(protein.vocab) 
+trg_vocab_size = 25#len(protein_trans.vocab) 
+embedding_size = 55#len(protein.vocab) #it should be 25. 21 amino, 2 start and end sequence, 1 for pad, and 1 for unknown token
+#EPOCHS 
+num_epochs =1000
+Unalign = False
+onehot=False
+wd=0.0
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+#             ilist.append(i)
+            
+
+
+onehot=False
+
+for i in family_list:
+    
+    modelpath = "TransSimple_fam"+str(i)+".pth.tar"
+    
+    pathTofile = pathtoFolder+ "combined_MSA_ddi_" +str(i)+"_joined.csv"
+    inputsize, outputsize = getLengthfromCSV(pathTofile)
+    os.path.isfile(pathTofile)
+    print("ddi", i, " is running")
+    name = "combined_MSA_ddi_" +str(i)+"_joined"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #Dataset
+    train_path = pathtoFolder + name +'_train.csv'
+    val_path = pathtoFolder + name +'_val.csv'
+    test_path = pathtoFolder + name +'_test.csv'
+    #add 2 for start and end token 
+    len_input = inputsize + 2
+    len_output =outputsize + 2
+    pds_train = ProteinTranslationDataset(train_path, device=device, Unalign=Unalign,filteringOption='and', returnIndex=True,onehot=onehot)
+    pds_test = ProteinTranslationDataset(test_path, device=device, Unalign=Unalign,filteringOption='and', returnIndex=True,onehot=onehot)
+    pds_val = ProteinTranslationDataset(val_path, device=device, Unalign=Unalign,filteringOption='and', returnIndex=True,onehot=onehot)
+
+    train_iterator = DataLoader(pds_train, batch_size=batch_size,
+                    shuffle=True, num_workers=0, collate_fn=default_collate)
+    test_iterator = DataLoader(pds_test, batch_size=batch_size,
+                    shuffle=True, num_workers=0, collate_fn=default_collate)
+    val_iterator = DataLoader(pds_val, batch_size=batch_size,
+                    shuffle=True, num_workers=0, collate_fn=default_collate)
+    
+    
+    # Model hyperparameters
+    src_pad_idx = pds_train.SymbolMap["<pad>"]#"<pad>"# protein.vocab.stoi["<pad>"] 
+    src_position_embedding = PositionalEncoding(embedding_size, max_len=len_input,device=device)
+    trg_position_embedding = PositionalEncoding(embedding_size, max_len=len_output, device=device)
+            
+    model = Transformer(
+        embedding_size,
+        src_vocab_size,
+        trg_vocab_size,
+        src_pad_idx,
+        num_heads,
+        num_encoder_layers,
+        num_decoder_layers,
+        forward_expansion,
+        dropout,
+        src_position_embedding,
+        trg_position_embedding,
+        device,
+        onehot=onehot,
+    ).to(device)
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    learning_rate = 5e-5
+    
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_normal_(p)
+            
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.0)
+    load_checkpoint(torch.load(modelpath), model, optimizer)
+
+
+    ### Create tempfiles
+    tempFile=next(tempfile._get_candidate_names())+".npy"
+    mode = "inter"
+    #### getlist
+    pdbtracker = pd.read_csv("pdbtracker.csv")
+    pdblist, chain1list, chain2list = getlists(pdbtracker, i)
+    np.save("pdblisttemp.npy", pdblist)
+    np.save("chain1listtemp.npy", chain1list)
+    np.save("chain2listtemp.npy", chain2list)
+    hmmRadical ="hmm_"+str(i)+"_"
+    tempTrain = writefastafrompds(pdsTrain)
+    output = subprocess.check_output(["julia", "contactPlot_merged.jl", tempTrain, "pdblisttemp.npy", "chain1listtemp.npy", "chain2listtemp.npy", hmmRadical, tempFile, mode])
+    print(output)
+    ppvO = np.load(tempFile)
+    
+    ### sample times 2
+    pds_sample = copy.deepcopy(pds_train)
+    batchIndex = makebatchList(len(pds_sample), 300)
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+    tempTrain = writefastafrompds(pds_sample)
+    output = subprocess.check_output(["julia", "contactPlot_merged.jl", tempTrain, "pdblisttemp.npy", "chain1listtemp.npy", "chain2listtemp.npy", hmmRadical, tempFile, mode])
+    print(output)
+    ppvS1 = np.load(tempFile)
+    
+        
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        # pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+        pds_sample.tensorOUT=torch.cat([pds_sample.tensorOUT,sampled.max(dim=2)[1] ],dim=1)
+        pds_sample.tensorIN=torch.cat([pds_sample.tensorIN,pds_sample.tensorIN[:,batchI] ], dim=1)
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        # pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+        pds_sample.tensorOUT=torch.cat([pds_sample.tensorOUT,sampled.max(dim=2)[1] ],dim=1)
+        pds_sample.tensorIN=torch.cat([pds_sample.tensorIN,pds_sample.tensorIN[:,batchI] ], dim=1)
+    tempTrain = writefastafrompds(pds_sample)
+    output = subprocess.check_output(["julia", "contactPlot_merged.jl", tempTrain, "pdblisttemp.npy", "chain1listtemp.npy", "chain2listtemp.npy", hmmRadical, tempFile, mode])
+    print(output)
+    ppvS3 = np.load(tempFile)
+        
+        
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        # pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+        pds_sample.tensorOUT=torch.cat([pds_sample.tensorOUT,sampled.max(dim=2)[1] ],dim=1)
+        pds_sample.tensorIN=torch.cat([pds_sample.tensorIN,pds_sample.tensorIN[:,batchI] ], dim=1)
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        # pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+        pds_sample.tensorOUT=torch.cat([pds_sample.tensorOUT,sampled.max(dim=2)[1] ],dim=1)
+        pds_sample.tensorIN=torch.cat([pds_sample.tensorIN,pds_sample.tensorIN[:,batchI] ], dim=1)
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        # pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+        pds_sample.tensorOUT=torch.cat([pds_sample.tensorOUT,sampled.max(dim=2)[1] ],dim=1)
+        pds_sample.tensorIN=torch.cat([pds_sample.tensorIN,pds_sample.tensorIN[:,batchI] ], dim=1)
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        # pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+        pds_sample.tensorOUT=torch.cat([pds_sample.tensorOUT,sampled.max(dim=2)[1] ],dim=1)
+        pds_sample.tensorIN=torch.cat([pds_sample.tensorIN,pds_sample.tensorIN[:,batchI] ], dim=1)
+    for batchI in batchIndex:
+        sampled = model.sample(pds_sample[batchI][0], max_len, nsample=1, method="simple")
+        # pds_sample.tensorOUT[:,batchI]=sampled.max(dim=2)[1]
+        pds_sample.tensorOUT=torch.cat([pds_sample.tensorOUT,sampled.max(dim=2)[1] ],dim=1)
+        pds_sample.tensorIN=torch.cat([pds_sample.tensorIN,pds_sample.tensorIN[:,batchI] ], dim=1)
+    tempTrain = writefastafrompds(pds_sample)
+    output = subprocess.check_output(["julia", "contactPlot_merged.jl", tempTrain, "pdblisttemp.npy", "chain1listtemp.npy", "chain2listtemp.npy", hmmRadical, tempFile, mode])
+    print(output)
+    ppvS8 = np.load(tempFile)
+    x = np.array(range(len(ppvO)))
+    plt.plot(x,ppvO, label="Original")
+    plt.plot(x,ppvS1, label="sampled*1")
+    plt.plot(x,ppvS3, label="sampled*3")
+    plt.plot(x,ppvS8, label="sampled*8")
+    plt.title("PPV" + str(i))
+    plt.legend()
+    plt.xscale("log")
+    plt.savefig("ppvS_"+str(i)+".png")
+
+
+
+
+    
+
+
+    
+
+
+    
+
+
+
+# for i in range(7000):
+#     original_pathj =  "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_"+str(i)+"_joined.fasta" 
+#     original_path1 =  "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_"+str(i)+"_1.fasta" 
+#     original_path2 =  "/home/feinauer/Datasets/DomainsInter/processed/combined_MSA_ddi_"+str(i)+"_2.fasta" 
+#     if os.path.isfile(original_pathj):
+#         print(i)
+#         subprocess.run(["hmmbuild", "--symfrac", "0.0", "hmm_"+str(i)+"_joined.hmm", original_pathj])
+#         subprocess.run(["hmmbuild", "--symfrac", "0.0", "hmm_"+str(i)+"_1.hmm", original_path1])
+#         subprocess.run(["hmmbuild", "--symfrac",  "0.0", "hmm_"+str(i)+"_1.hmm", original_path2])
+
+
+
+
+
+
+
+
+
+
+
