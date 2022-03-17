de2f2940929c852b5c4c03d41c51c02c9e235625
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-03-17 20:02:36 +0100

    hypertest

--- hyperparameterSweep.py
+++ hyperparameterSweep.py
@@ -107,7 +107,7 @@ def train(config=None):
         Unalign = False
         count=0
         if datasettype == "Domains":
-            pathtoFolder = "/home/feinauer/Datasets/DomainsInter/processed/"#"/home/Datasets/DomainsInter/processed/"
+            pathtoFolder = "/home/Datasets/DomainsInter/processed/"#"/home/feinauer/Datasets/DomainsInter/processed/"#
             pathTofile = pathtoFolder+ "combined_MSA_ddi_" +str(i)+"_joined.csv"
             inputsize, outputsize = getLengthfromCSV(pathTofile)
             os.path.isfile(pathTofile)
