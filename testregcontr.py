04223282e71d329a16c9ecee1007e4d6997b6f43
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-03-18 17:52:10 +0100

    compareREG

--- testregcontr.py
+++ testregcontr.py
@@ -48,9 +48,9 @@ dropout = 0.10
 wd = 0.5
 alpha = 0.0
 ##### Training simple 
-#pathtoFolder = "/home/feinauer/Datasets/DomainsInter/processed/"
+pathtoFolder = "/home/feinauer/Datasets/DomainsInter/processed/"
 #pathtoFolder = "/home/meynard/Datasets/DomainsInter/processed/" ##Jussieu GPU
-pathtoFolder = "/home/Datasets/DomainsInter/processed/"
+#pathtoFolder = "/home/Datasets/DomainsInter/processed/"
 pathTofile = pathtoFolder+ "combined_MSA_ddi_" +str(i)+"_joined.csv"
 
 inputsize, outputsize = getLengthfromCSV(pathTofile)
@@ -280,8 +280,187 @@ for epoch in range(num_epochs+1):
 
 wandb.finish()
 
+#####WD0
+
+wd = 0.0
+
+src_pad_idx = pds_train.SymbolMap["<pad>"]#"<pad>"# protein.vocab.stoi["<pad>"] 
+src_position_embedding = PositionalEncoding(embedding_size, max_len=len_input,device=device)
+trg_position_embedding = PositionalEncoding(embedding_size, max_len=len_output, device=device)
+        
+model = Transformer(
+    embedding_size,
+    src_vocab_size,
+    trg_vocab_size,
+    src_pad_idx,
+    num_heads,
+    num_encoder_layers,
+    num_decoder_layers,
+    forward_expansion,
+    dropout,
+    src_position_embedding,
+    trg_position_embedding,
+    device,
+    onehot=onehot,
+).to(device)
+
+#whyyy 'cpu?'
+
+config_dict = {
+  "num_layers": num_encoder_layers,
+  "embedding":embedding_size,
+  "forward_expansion": forward_expansion,
+  "batch_size": batch_size,
+  "Encoder": "Positional",
+  "Family":i,
+  "dropout":dropout,
+  "len input":len_input,
+  "len output":len_output,
+  "sizetrain": len(pds_train),
+  "sizeval": len(pds_val),
+  "num_heads": num_heads,
+  "loss": "CE",
+  "wd":wd,
+  "alpha": alpha,
+}
+wandb.config.update(config_dict) 
+
+step = 0
+step_ev = 0
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+learning_rate = 5e-5
+
+for p in model.parameters():
+    if p.dim() > 1:
+        nn.init.xavier_normal_(p)
+        
+        
+    
+optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=wd)
+pad_idx = "<pad>"#protein.vocab.stoi["<pad>"]
+criterion = nn.CrossEntropyLoss(ignore_index=pds_train.SymbolMap["<pad>"])
+
+for epoch in range(num_epochs+1):
+    print(f"[Epoch {epoch} / {num_epochs}]")
+    model.train()
+    lossesCE = []
+    accuracyTrain = 0
+    for batch_idx, batch in enumerate(train_iterator):
+        inp_data, target= batch[0], batch[1]
+        output = model(inp_data, target[:-1, :])
+        accuracyTrain += accuracy(batch, output, onehot=False).item()
+        output = output.reshape(-1, output.shape[2])#keep last dimension
+        if onehot:
+            _, targets_Original = target.max(dim=2)
+        else:
+            targets_Original= target
+        targets_Original = targets_Original[1:].reshape(-1)
+        optimizer.zero_grad()
+        loss = criterion(output, targets_Original)
+        lossesCE.append(loss.item())
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1) 
+        optimizer.step()
+        
+        
+    mean_lossCETrain = sum(lossesCE) / len(lossesCE)
+    accuracyTrain = accuracyTrain/ntrain
+    # mean_lossMatchingTrain = sum(lossesMatching) / len(lossesMatching)
+    step += 1
+    # scheduler.step(mean_lossCETrain)
+    model.eval()
+    lossesCE_eval = []
+    lossesMatching_eval = []
+    accuracyVal = 0
+    
+    if epoch%1==0:
+        with  torch.no_grad():
+            for batch_idx, batch in enumerate(val_iterator):
+                inp_data, target= batch[0], batch[1]
+                inp_data = inp_data.to(device)
+                output = model(inp_data, target[:-1, :])
+                accuracyVal += accuracy(batch, output, onehot=False).item()
+                output = output.reshape(-1, output.shape[2]) #keep last dimension
+                if onehot:
+                    _, targets_Original = target.max(dim=2)
+                else:
+                    targets_Original= target
+                targets_Original = targets_Original[1:].reshape(-1)
+                loss_eval = criterion(output, targets_Original)
+                lossesCE_eval.append(loss_eval.item()) 
+            mean_lossVal = sum(lossesCE_eval) / len(lossesCE_eval)
+            accuracyVal = accuracyVal/nval
+            step_ev +=1
+    #wandb.log({"Train loss CE": mean_lossCETrain,  "Val loss CE": mean_lossVal,  "accuracyVal":accuracyVal ,  "accuracyTrain": accuracyTrain, "epoch":epoch})#,"Val Loss Matching":mean_lossMatchingVal, "alpha":alpha "Train loss Matching": mean_lossMatchingTrain,
+    
+    
+    lossesCE_test = []
+    lossesMatching_test = []
+    accuracytest = 0
+    
+    if epoch%1==0:
+        with  torch.no_grad():
+            for batch_idx, batch in enumerate(test_iterator):
+                inp_data, target= batch[0], batch[1]
+                inp_data = inp_data.to(device)
+                output = model(inp_data, target[:-1, :])
+                accuracytest += accuracy(batch, output, onehot=False).item()
+                output = output.reshape(-1, output.shape[2]) #keep last dimension
+                if onehot:
+                    _, targets_Original = target.max(dim=2)
+                else:
+                    targets_Original= target
+                targets_Original = targets_Original[1:].reshape(-1)
+                loss_test = criterion(output, targets_Original)
+                lossesCE_test.append(loss_test.item()) 
+            mean_losstest = sum(lossesCE_test) / len(lossesCE_test)
+            accuracytest = accuracytest/ntest
+            step_ev +=1
+    wandb.log({"Train loss CE": mean_lossCETrain,  "Val loss CE": mean_lossVal, "test loss CE": mean_losstest,  "accuracyVal":accuracyVal , "accuracytest":accuracytest ,  "accuracyTrain": accuracyTrain, "epoch":epoch})
+    
+    
+    if epoch%200==0:
+        criterionE = nn.CrossEntropyLoss(ignore_index=pds_train.SymbolMap["<pad>"], reduction='none')
+        model.eval()
+        criterionE = nn.CrossEntropyLoss(ignore_index=pds_train.SymbolMap["<pad>"], reduction='none')
+        scoreHungarianVal = HungarianMatchingBS(pds_val, model,100)
+        scoHVal = scipy.optimize.linear_sum_assignment(scoreHungarianVal)
+        scoreMatchingVal = sum(scoHVal[0]==scoHVal[1])
+        scoreMatchingValClose = sum((scoHVal[0]==scoHVal[1])[maskValclose])
+        scoreMatchingValFar = sum((scoHVal[0]==scoHVal[1])[maskValfar])
+        # scoreHungarianTrain = HungarianMatchingBS(pds_train, model,100)
+        # scoHTrain = scipy.optimize.linear_sum_assignment(scoreHungarianTrain)
+        # scoreMatchingTrain = sum(scoHTrain[0]==scoHTrain[1])
+        wandb.log({ "scoreMatching Val": scoreMatchingVal, "scoreMatchingValClose": scoreMatchingValClose, "scoreMatchingVal Far": scoreMatchingValFar,"epoch":epoch})
+        
+    if epoch%1000 ==0:
+        mode = "inter"
+        #### getlist
+        pdbtracker = pd.read_csv("pdbtracker.csv")
+        pdblist, chain1list, chain2list = getlists(pdbtracker, i)
+        hmmRadical =pathtoFolder+"hmm_"+str(i)+"_"
+    
+        ppvO = PPV_from_pds(pds_train, pdblist, chain1list, chain2list, hmmRadical, mode ="inter")
+        
+        
+        sampled = sampleDataset(model, pds_train, len_output, multiplicative =5)
+        ppv = PPV_from_pds(sampled, pdblist, chain1list, chain2list, hmmRadical, mode ="inter")
+            
+            
+        x_values = np.array(range(1,len(ppv)+1))
+        data = [[x, y] for (x, y) in zip(x_values, ppv)]
+        table = wandb.Table(data=data, columns = ["x", "y"])
+        wandb.log({"PPV"+str(epoch)+"_"+str(i) : wandb.plot.line(table, "x", "y",
+                   title="Custom Y vs X Line Plot"), "epoch":epoch})
+
+    
+
+wandb.finish()
 
 
+
+####### REG
 
 
 
