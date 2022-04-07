a20c256fb225b2b60c60c8c04fd919a29fa40923
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-04-07 19:32:09 +0200

    NewReyni

--- testNewReyni.py
+++ testNewReyni.py
@@ -49,8 +49,8 @@ wd = 0.5
 alphalist = [-0.1, -0.5, -0.7]
 ##### Training simple 
 #pathtoFolder = "/Data/DomainsInter/processed/"
-#pathtoFolder = "/home/meynard/Datasets/DomainsInter/processed/" ##Jussieu GPU
-pathtoFolder = "/home/Datasets/DomainsInter/processed/"
+pathtoFolder = "/home/meynard/Datasets/DomainsInter/processed/" ##Jussieu GPU
+#pathtoFolder = "/home/Datasets/DomainsInter/processed/"
 pathTofile = pathtoFolder+ "combined_MSA_ddi_" +str(i)+"_joined.csv"
 
 inputsize, outputsize = getLengthfromCSV(pathTofile)
