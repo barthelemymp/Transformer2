28c8b238599d9f4749a88af3a4c88e7fb6967065
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-05-03 15:39:50 +0200

    plotdiff

--- plotsdiff.py
+++ plotsdiff.py
@@ -219,7 +219,7 @@ for i in ilist:# range(1000,1500):
         plt.title("Cross Entropy Loss at different distance from trainset for PF03171_PF14226", fontsize=18)
         plt.scatter(x,y, alpha=0.3, color="blue", label="Transformer")
         plt.scatter(x,y2, alpha=0.3, color="orange", label="ardca")
-        plt.scatter(x,y2, alpha=0.3, color="green", label="Reyni")
+        plt.scatter(x,y3, alpha=0.3, color="green", label="Reyni")
         plt.tick_params(axis='both', labelsize=18)
         plt.legend(fontsize=18)
         plt.savefig("distanceCE_2compare"+str(i)+".pdf")
