b7b26fb1cce138abb28360e11e2beb0c96ccc411
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-05-16 21:25:13 +0200

    fontsize

--- plotsdiff.py
+++ plotsdiff.py
@@ -302,18 +302,18 @@ for i in ilist:# range(1000,1500):
         labs = [l.get_label() for l in lns]
 
         # plt.xlabel("Hamming Distance from Training Set", fontsize=25)
-        ax1.set_ylabel("$\mathcal{PP}$ - Perplexity", fontsize=25)
+        ax1.set_ylabel("$\mathcal{PP}$ - Perplexity", fontsize=35)
         # plt.title("Perplexity at Different Distance-  from Training Set for"+famname, fontsize=25)
-        ax1.set_xlabel("Hamming Distance from Training Set", fontsize=25)
-        ax2.set_ylabel("$\mathcal{A}$ - Accuracy", fontsize=25)
-        plt.title("Perplexity and Accuracy at Different Distance from Training Set for"+famname, fontsize=25)
+        ax1.set_xlabel("Hamming Distance from Training Set", fontsize=35)
+        ax2.set_ylabel("$\mathcal{A}$ - Accuracy", fontsize=35)
+        plt.title("Perplexity and Accuracy at Different Distance from Training Set for"+famname, fontsize=35)
 
         
         
         #plt.scatter(x,y3, alpha=0.3, color="green", label="Reyni")
-        ax1.tick_params(axis='both', labelsize=25)
-        ax2.tick_params(axis='both', labelsize=25)
-        ax1.legend(lns, labs,fontsize=18, loc=6)
+        ax1.tick_params(axis='both', labelsize=35)
+        ax2.tick_params(axis='both', labelsize=35)
+        ax1.legend(lns, labs,fontsize=35, loc=6)
         # ax1.legend(fontsize=25)
         plt.savefig("distance_compareMerged"+str(i)+".pdf",bbox_inches='tight')
         #plt.savefig("distance_comparePP"+str(i)+".pdf",bbox_inches='tight')
