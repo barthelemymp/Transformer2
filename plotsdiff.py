7fca0a56ec3b09936d0cd9cbd452309cfa80f984
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-05-23 09:15:10 +0200

    newplots

--- plotsdiff.py
+++ plotsdiff.py
@@ -302,18 +302,18 @@ for i in ilist:# range(1000,1500):
         labs = [l.get_label() for l in lns]
 
         # plt.xlabel("Hamming Distance from Training Set", fontsize=25)
-        ax1.set_ylabel("$\mathcal{PP}$ - Perplexity", fontsize=37)
+        ax1.set_ylabel("$\mathcal{PP}$ - Perplexity", fontsize=40)
         # plt.title("Perplexity at Different Distance-  from Training Set for"+famname, fontsize=25)
-        ax1.set_xlabel("Hamming Distance from Training Set", fontsize=37)
-        ax2.set_ylabel("$\mathcal{A}$ - Accuracy", fontsize=37)
-        plt.title("$\mathcal{PP}$ & $\mathcal{A}$ Distribution "+famname, fontsize=37)
+        ax1.set_xlabel("Hamming Distance from Training Set", fontsize=40)
+        ax2.set_ylabel("$\mathcal{A}$ - Accuracy", fontsize=40)
+        plt.title("$\mathcal{PP}$ & $\mathcal{A}$ Distribution "+famname, fontsize=40)
 
         
         
         #plt.scatter(x,y3, alpha=0.3, color="green", label="Reyni")
-        ax1.tick_params(axis='both', labelsize=37)
-        ax2.tick_params(axis='both', labelsize=37)
-        ax1.legend(lns, labs,fontsize=33, loc=(0.01,0.2))
+        ax1.tick_params(axis='both', labelsize=40)
+        ax2.tick_params(axis='both', labelsize=40)
+        ax1.legend(lns, labs,fontsize=35, loc=(0.01,0.2))
         # ax1.legend(fontsize=25)
         plt.savefig("distance_compareMerged"+str(i)+".pdf",bbox_inches='tight')
         #plt.savefig("distance_comparePP"+str(i)+".pdf",bbox_inches='tight')
